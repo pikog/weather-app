d9f1899ea4324b65a7c0b7375cc9242b8c822c3d
Pikog <perso.pikog@gmail.com>
2018-02-01 00:05:29 +0100

    :zap: Error handling

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const yargsConfig = require('./yargs-config.js')
 //Configuration for yargs
 const argv = yargsConfig()
 
-console.log(argv)
-
+//Request Google Maps API to get latitude and longitude in function to an address
 request(
     {
         url: 'https://maps.googleapis.com/maps/api/geocode/json',
@@ -18,9 +17,28 @@ request(
         json: true
     }, (error, response, body) =>
     {
-        console.log(JSON.stringify(response, undefined, 2))
-        console.log(`Address: ${body.results[0].formatted_address}`)
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+        //Connection error handling
+        if(error)
+        {
+            console.log('Unable to connect')
+        }
+        //No result error handling
+        else if(body.status === 'ZERO_RESULT')
+        {
+            console.log('Invalid Address')
+        }
+        //Good response
+        else if(body.status === 'OK')
+        {
+            console.log(JSON.stringify(response, undefined, 2))
+            console.log(`Address: ${body.results[0].formatted_address}`)
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+        }
+        //Unknow error handling
+        else
+        {
+            console.log('Error with Google response')
+        }
     }
 )
