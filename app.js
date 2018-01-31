a3894f53363612c8ca58fa1620ed8a45c1035feb
Pikog <perso.pikog@gmail.com>
2018-02-01 00:32:23 +0100

    :building_construction:Refactoring code

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
-//Import package
-const request = require('request')
-
 //Import our files
-const yargsConfig = require('./yargs-config.js')
+const yargsConfig = require('./yargs-config')
+const geocode = require('./geocode/geocode')
 
 //Configuration for yargs
 const argv = yargsConfig()
 
-//Request Google Maps API to get latitude and longitude in function to an address
-request(
+geocode(argv.address, (error, results) =>
+{
+    if(error)
     {
-        url: 'https://maps.googleapis.com/maps/api/geocode/json',
-        qs: {
-            address: argv.address,
-        },
-        json: true
-    }, (error, response, body) =>
-    {
-        //Connection error handling
-        if(error)
-        {
-            console.log('Unable to connect')
-        }
-        //No result error handling
-        else if(body.status === 'ZERO_RESULT')
-        {
-            console.log('Invalid Address')
-        }
-        //Good response
-        else if(body.status === 'OK')
-        {
-            console.log(JSON.stringify(response, undefined, 2))
-            console.log(`Address: ${body.results[0].formatted_address}`)
-            console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-            console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
-        }
-        //Unknow error handling
-        else
-        {
-            console.log('Error with Google response')
-        }
+        console.log(error)
     }
-)
+    else
+    {
+        console.log(JSON.stringify(results, undefined, 2))
+    }
+})
---
+++ geocode/geocode.js
@@ -0,0 +1,42 @@
+//Import package
+const request = require('request')
+
+//Function to Request Google Maps API in order to get latitude and longitude in function to an address
+module.exports = (address, callback) =>
+{
+    request(
+        {
+            url: 'https://maps.googleapis.com/maps/api/geocode/json',
+            qs: {
+                address
+            },
+            json: true
+        }, (error, response, body) =>
+        {
+            //Connection error handling
+            if(error)
+            {
+                callback('Unable to connect')
+            }
+            //No result error handling
+            else if(body.status === 'ZERO_RESULT')
+            {
+                callback('Invalid Address')
+            }
+            //Good response
+            else if(body.status === 'OK')
+            {
+                callback(undefined, {
+                    address: body.results[0].formatted_address,
+                    lat: body.results[0].geometry.location.lat,
+                    lng: body.results[0].geometry.location.lng
+                })
+            }
+            //Unknow error handling
+            else
+            {
+                callback('Error with Google response')
+            }
+        }
+    )
+}
